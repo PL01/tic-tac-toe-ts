f9356662df6d0d0f46d4e90981d757cceead3ce2
Pablo Lara <pablolara01@yahoo.com>
2024-03-10 15:54:57 -0400

    Added sq classname for X or O values

--- src/Square.tsx
+++ src/Square.tsx
@@ -16,9 +16,17 @@ interface SquareProps{ //Interface helps enforce the type check in our component
 
 // Definition of an individual square on the board.
 const Square: React.FunctionComponent<SquareProps> = ({value, onClick}) => {
+    
+    //sq is a dynamic class name based on the square's value, X or O or null using conditional operators
+    const sq = value === SquareXO.X ? 'square x-square' : value === SquareXO.O ? 'square o-square' : 'square';
+    /*  First condition: value === SquareXO.X, if true, the expression after the ? will execute
+        Second condition: value === SquareXO.O, if true, the expression after the ? will execute
+        Default condition: If  neither of the above conditions are true, the square is null/empty
+        and default class name is 'square'.
+    */
     return(
         // We make a button with a speccific class and onClick handler
-        <button className="square" onClick={onClick}>
+        <button className={sq} onClick={onClick}>
             {value}
             {/* Display the value ('X', 'O', or null) when square is clicked. */}
         </button>
